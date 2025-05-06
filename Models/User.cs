60d51801099dcde552f8f51068ff88ca8831d4db
ouahchiam <mouahchia@outlook.com>
2025-05-06 18:16:15 +0100

    Change User model

--- Models/User.cs
+++ Models/User.cs
@@ -6,9 +6,7 @@
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public List<string> OwnedParkingSlots { get; set; } = new List<string>();
-        public List<string> PaymentIntents { get; set; } = new List<string>();
-        public List<string> ParkingLots { get; set; } = new List<string>();
+        public int OverdueBalance { get; set; } = 0;
+        public string LicensePlate { get; set; } = string.Empty;
     }
 }
